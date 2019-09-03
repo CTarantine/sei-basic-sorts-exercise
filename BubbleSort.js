f04932bfe0c947005fc6d366417693361e84573d
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-09-02 22:29:16 -0400

    bubble sort complete

--- BubbleSort.js
+++ BubbleSort.js
@@ -2,17 +2,28 @@ function bubbleSort(array) {
     // Make some sort of loop that keeps happening if there were any swaps
     // that happened this turn
 
-    while(){}
+    for (let i = 0; i < array.length; i++) {
 
-    // reset swapHappened to false each time so we can detect if a swap
-    // happened in this specific iteration.
-
-    // Make another loop (inside the first one) to go through one 
-    // round of swapping from the start of the list to the end
-    
-    // Inside inner loop:
-    // compare each pair of elements near each other
-    // swap them if the bigger one was at a lower index.
+        if (array[i] > array[i + 1]) {
+            i++;
+            // reset swapHappened to false each time so we can detect if a swap
+            // happened in this specific iteration.
+            swapHappened = false;
+        }
+        // Make another loop (inside the first one) to go through one 
+        // round of swapping from the start of the list to the end
+        for (let j = 0; j < array.length; j++) {
+            // Inside inner loop:
+            // compare each pair of elements near each other
+            // swap them if the bigger one was at a lower index.
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapHappened= true;
+            }
+        }
+    }
 
     // Make sure to keep track of whether a swap happened!
 
@@ -21,3 +32,5 @@ function bubbleSort(array) {
 }
 
 module.exports = bubbleSort;
+
+console.log(bubbleSort([8, 22, 16, 5, 30]))
